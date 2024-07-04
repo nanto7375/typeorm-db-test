174c2ba1430eb9d462bf3471a2e51bb60c3b24a0
seungwoo <jsu7375@gmail.com>
2024-07-04 23:52:49 +0900

    feat: joosuk kk

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // curl로 편하게 요청하려고 그냥 get 사용
   @Get('set')
   setData() {
     return this.appService.setData();
